50f77a1e7948147469e061faf6f9d8dd73c33d64
Ibrahim Raimi <raimiibrahim44@gmail.com>
2024-01-16 23:47:42 +0100

    site configurations for enhanced performance and reliability.

---
+++ configs/site.ts
@@ -0,0 +1,13 @@
+import { SiteConfig } from "@/types";
+
+export const siteConfig: SiteConfig = {
+  name: "Craftform",
+  description:
+    "Craftform – the innovative form builder that redefines your business. Bid adieu to dull forms and welcome a dynamic approach to glean valuable insights.",
+  url: "https://craftform.vercel.app",
+  ogImage: "https://craftform.vercel.app/opengraph-image.png",
+  links: {
+    twitter: "https://twitter.com/ibrahimraimi_",
+    github: "https://github.com/ibrahimraimi/craftform",
+  },
+};
