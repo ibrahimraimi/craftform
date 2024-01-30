f166813eee121bb36b5a91fcf95f6461eb7ca6c9
Ibrahim Raimi <raimiibrahim44@gmail.com>
2024-01-30 03:16:05 +0100

    fix invalid default export

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -3,24 +3,15 @@ import { currentUser } from "@clerk/nextjs";
 import { SiteFooter } from "@/components/site-footer";
 import { SiteHeader } from "@/components/site-header";
 
-interface MarketingLayoutProps
-  extends React.PropsWithChildren<{
-    modal: React.ReactNode;
-  }> {}
-
 export default async function MarketingLayout({
   children,
-  modal,
-}: MarketingLayoutProps) {
+}: React.PropsWithChildren) {
   const user = await currentUser();
 
   return (
     <div className="relative flex min-h-screen flex-col">
       <SiteHeader user={user} />
-      <main className="flex-1">
-        {children}
-        {modal}
-      </main>
+      <main className="flex-1">{children}</main>
       <SiteFooter />
     </div>
   );
